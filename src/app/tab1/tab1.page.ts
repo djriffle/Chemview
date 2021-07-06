9608b1b4c797308c0cca64fa4269c638e5ea5559
djriffle <djriffle1@gmail.com>
2021-07-05 23:54:12 -0400

    added smiles view

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
+import { NumberSymbol } from '@angular/common';
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+const NGL = require('ngl/dist/ngl.js')
+const SmilesDrawer = require('smiles-drawer/app.js')
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,36 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  deviceWidth: number;
+  deviceHeight: number;
+
+  constructor(
+    private platform: Platform,
+  ) {
+    this.deviceHeight = platform.height()
+    this.deviceWidth = platform.width()
+  }
+
+  ngOnInit(){
+
+    var stage = new NGL.Stage("3d",{ backgroundColor: "white" });
+    //stage.loadFile("rcsb://1crn", {defaultRepresentation: true});
+    stage.loadFile( "http://files.rcsb.org/download/1crn.cif" ).then( function( comp ){
+      comp.addRepresentation( "surface", { multipleBond: true } );
+    } );
+    stage.mouseControls.add("drag-left+right", NGL.MouseActions.zoomDrag);
+
+    // let options = {
+    //   width: this.deviceWidth,
+    //   height: this.deviceHeight
+    // };
+    // let smilesDrawer = new SmilesDrawer.Drawer(options);
+    // SmilesDrawer.parse('COC(=O)C1C(CC2N(C1CC2)C)OC(=O)c1ccccc1', function (tree) {
+    //   smilesDrawer.draw(tree, 'twod', 'light', false);
+    //   }, function (err) {
+    //       console.log(err);
+    //   })
+    }
+  
 
 }
