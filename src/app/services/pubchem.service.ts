e7ee0aca94858941e38468fb46c1db36a6ad6d68
djriffle <djriffle1@gmail.com>
2021-07-06 22:25:49 -0400

    added isomeric smile support

--- src/app/services/pubchem.service.ts
+++ src/app/services/pubchem.service.ts
@@ -9,9 +9,17 @@ export class PubchemService {
 
 
   async getSmiles(query:string){
-    //TODO right now this gets isomers there should be an option to get the non isomer
+    //Gets the canonical
     let chemJson = await this.http.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/'+query+'/json').toPromise()
     let smiles = await chemJson["PC_Compounds"][0].props[18].value.sval
+    console.log(chemJson)
+    return await smiles
+  }
+
+  async getSmilesIsomeric(query:string){
+    //Gets the isomeric smile
+    let chemJson = await this.http.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/'+query+'/json').toPromise()
+    let smiles = await chemJson["PC_Compounds"][0].props[19].value.sval
     console.log(smiles)
     return await smiles
   }
