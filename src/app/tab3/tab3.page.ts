53254bcca49d833dd9cb56875d79f00aed61d2e4
djriffle <djriffle1@gmail.com>
2021-07-09 17:13:20 -0500

    favorites page ui

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FavoriteService } from '../services/favorite.service';
+import { CurrentChemService } from '../services/current-chem.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  favorites
+  empty:boolean = true
 
-  constructor() {}
+  //TODO add admob integration
+  constructor(private favoriteService:FavoriteService, private currentChemService:CurrentChemService,private router:Router) {}
+
+  ngOnInit(){
+    this.favorites = this.favoriteService.getFavorites()
+    this.empty = this.favorites.length == 0
+  }
+
+  removeFavorite(favorite){
+    console.log(favorite)
+    this.favoriteService.removeFavorite(favorite)
+    this.favorites = this.favoriteService.getFavorites()
+  }
+
+  load(favorite){
+    this.currentChemService.setName(favorite)
+    this.router.navigate(['tabs/tab1'])
+  }
 
 }
